49277e88d62fda8934cddac15cd9015b15f8a50b
Tariq Darwish <darwishtariq21@gmail.com>
2017-02-22 12:22:31 -0500

    promises

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,55 @@
 window.onload = function () {
 
-  //instantiate a new XML object
-  var http = new XMLHttpRequest();
-  //store the url as a string in a variable
-  var url = 'http://pokeapi.co/api/v2/pokemon-habitat/1/';
+//declared a function with one parameter called url
+  function get(url) {
+    //as soon as the function runs, return a promist object, we leverage reject and resolve to reject
+    //and accept data that is or isnt successfully retrieved.
+    return new Promise(function(resolve,reject) {
+      //instantiate the AJAX object
+      var xhttp = new XMLHttpRequest();
+      //request  data with the three parameters passed into the open method.
+      xhttp.open('GET', url, true);
+      //upon loading the data we specify a conditional to make sure the request was a success,
+      //if it is we turn the response in to JSON
+      //if not we grab the text status which will give us an idea to what the error was and why
+      //the data was not successfully retrieved.
+      xhttp.onload = function (){
+        if( xhttp.status == 200 ) {
+          resolve( JSON.parse(xhttp.response));
+        } else {
+          reject(xhttp.statusText);
+        }
+      };
+      xhttp.onerror = function () {
+        reject(xhttp.statusText);
+      };
+      //send the Data back
+      xhttp.send();
+    });
+  }
 
-  //use method .open() to request the data and pass it the type of http request, the destination, and a boolean value for asynchronous runtime.
-  http.open('GET', url, true);
-  http.send();
+//now here we call the get function and store it into a variable called promise.
+  var promise = get('http://pokeapi.co/api/v2/pokemon/');
+  //then we call the method 'then' so we can do things with this data. so we pass it a function with
+//  a parameter to retrieve the data being passed. and logging out the data to the console.
+    promise.then(function(pokeData) {
+      console.log(pokeData);
+      return get('http://pokeapi.co/api/v2/pokedex/14/');
+      //we can also chain multiple AJAX requests or callback functions with promises by asking
+      //the previous then method to return the invocation of another request. and we also must do another .then
+      //method to handle the data that is being sent back for the second request.
+    }).then(function(pokeData2) {
+      console.log(pokeData2);
+      //the method catch handles all rejected cases, if any, of object Promise
+    }).catch(function(error) {
+      console.log(error);
+    });
 
-  http.onreadystatechange = function () {
-    if( http.readyState == 4 && http.status == 200 ) {
+    var a;
+    //goes out to the internet and looks for a value
 
-      var data = JSON.parse( http.response );
-      var pokemon = data["pokemon_species"];
-
-      for(var i = 0; i < pokemon.length; i +=1) {
-        console.log(pokemon[i]["name"]);
-      }
+    if ( a ) {
+      console.log('something is there');
     }
-  };
 
-
-  // console.log("This is an AJAX request ", http);
-
-
-  /*
-    READY STATES
-
-    0 - request is not initialized
-    1 - request has been set up
-    2 - request has been sent
-    3 - request is in progress
-    4 - request is complete
-
-  */
-
-};
+}
